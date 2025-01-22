be6062ddeb1f9ec80364d7fb983da0d8931adb4f
LevKudrashow <levkudrashow@gmail.com>
2025-01-22 20:30:48 +0300

    Доработка чатбота, изменение уведомлений
    Прикрепление файла со всеми неправильными темами для каждого преподователя

--- bot public.py
+++ bot public.py
@@ -12,11 +12,9 @@ teachers = {'1': 1,
 
 current_df = {}
 
-
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, "Для работы бота отправьте файл 'Отчет по темам занятий.xls'")
-
+    bot.send_message(message.chat.id, "Данный бот выполняет задания 3 и 7 - проверка заполнений тем урока и подсчет пар у выбранной/ых групп соответсвенно.\nДля работы бота отправьте файл 'Отчет по темам занятий.xls'")
 
 @bot.message_handler(content_types=['document'])
 def handle_document(message):
@@ -28,7 +26,7 @@ def handle_document(message):
     current_df[message.from_user.id] = df
 
     unique_groups = df['Группа'].unique()
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=3)
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     markup.add(types.KeyboardButton("Завершить работу бота"))
     for group in unique_groups:
         markup.add(types.KeyboardButton(group))
@@ -41,13 +39,12 @@ def handle_document(message):
         markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
         markup.add(types.KeyboardButton("Да"), types.KeyboardButton("Нет"))
         bot.send_message(message.chat.id,
-                         "Было найдено " + misspelled.__str__() + " неправильно введенных тем уроков. Стоит ли уведомить преподавателей?",
+                         "Было найдено " + str(misspelled) + " неправильно введенных тем уроков. Стоит ли уведомить преподавателей?",
                          reply_markup=markup)
         bot.register_next_step_handler(message, process_notify_step)
     else:
-        bot.send_message(message.chat.id, "Выберите группу:", reply_markup=markup)
+        bot.send_message(message.chat.id, "Теперь вы можете посмотреть количество пар по каждому предмету у выбираемой/ых групп. Выберите группу:", reply_markup=markup)
         bot.register_next_step_handler(message, process_group_step)
-
 
 def process_notify_step(message):
     if message.text.strip().upper() == "ДА":
@@ -55,37 +52,33 @@ def process_notify_step(message):
         df = current_df.get(user_id)
 
         notifications = {}
+        incorrect_entries = {}
 
         for index, row in df.iterrows():
             if row['Тема урока'] and not re.match(r'^Урок №\d+\. Тема:.*', row['Тема урока']):
                 teacher_name = row['ФИО преподавателя']
+                if teacher_name not in incorrect_entries:
+                    incorrect_entries[teacher_name] = []
 
-                if teacher_name not in notifications:
-                    notifications[teacher_name] = []
-
-                notifications[teacher_name].append(row['Тема урока'])
+                incorrect_entries[teacher_name].append(row['Тема урока'])
 
         notificated = 0
-        for teacher_name, issues in notifications.items():
-            lenIssues = len(issues)
-            limited_issues = issues[:5]
-            message_text = f"Здравствуйте, {teacher_name}, я бот.\nХочу уведомить вас о том, что вы неправильно ввели тему урока. Она должна быть записана как:\n\"Урок №(число). Тема: (текст).\"\n\n"
-            if lenIssues > 5:
-                message_text += f"Примеры ошибочных тем:\n{'\n'.join(limited_issues)}"
-            else:
-                message_text += f"Ошибочные темы:\n{'\n'.join(limited_issues)}"
+        for teacher_name, issues in incorrect_entries.items():
+            text_filename = f"{teacher_name.replace(' ', '_')}_неправильные_темы.txt"
+            with open(text_filename, 'w', encoding='utf-8') as f:
+                for issue in issues:
+                    f.write(issue + '\n')
 
+            message_text = f"Здравствуйте, {teacher_name}, я бот.\nХочу уведомить вас о том, что у вас неправильно введенные темы уроков.\nПрикреплён файл с неправильными темами."
             try:
                 bot.send_message(teachers[teacher_name], message_text)
+                with open(text_filename, 'rb') as file_to_send:
+                    bot.send_document(teachers[teacher_name], file_to_send, caption="Неправильные темы")
+                notificated += 1
             except KeyError:
                 bot.send_message(message.chat.id,
-                                 f"Преподователя \"{teacher_name}\" нет в базе.\nСообщение которое должно было отправиться:")
-            else:
-                bot.send_message(message.chat.id, "Отправленное сообщение:")
-            finally:
-                bot.send_message(message.chat.id, message_text)
-
-        if notificated < notifications.items().__len__():
+                                 f"Преподавателя \"{teacher_name}\" нет в базе.\nСообщение не было отправленно.")
+        if notificated < len(notifications):
             bot.send_message(message.chat.id, "Не все преподаватели были уведомлены.")
         else:
             bot.send_message(message.chat.id, "Преподаватели уведомлены.")
@@ -101,15 +94,14 @@ def process_notify_step(message):
     for group in unique_groups:
         markup.add(types.KeyboardButton(group))
 
-    bot.send_message(message.chat.id, "Выберите группу:", reply_markup=markup)
+    bot.send_message(message.chat.id, "Теперь вы можете посмотреть количество пар по каждому предмету у выбираемой/ых групп. Выберите группу:", reply_markup=markup)
     bot.register_next_step_handler(message, process_group_step)
-
 
 def process_group_step(message):
     if message.text == "Завершить работу бота":
         markup = types.ReplyKeyboardRemove()
         bot.send_message(message.chat.id, "Кнопки удалены.", reply_markup=markup)
-        bot.send_message(message.chat.id, "Работа бота завершена. Если хотите начать заново, введите /start.")
+        bot.send_message(message.chat.id, "Работа бота завершена. Если хотите начать заново, введите либо нажмите на /start.")
         return
 
     user_id = message.from_user.id
@@ -140,7 +132,6 @@ def process_group_step(message):
 
     bot.send_message(message.chat.id, "Выберите другую группу:", reply_markup=markup)
     bot.register_next_step_handler(message, process_group_step)
-
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
